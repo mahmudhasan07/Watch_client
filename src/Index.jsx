379e1a731c908104b19f413a2e5c15c5c026f6eb
Mahmudhasan <mahmudhasan.hb@gmail.com>
2024-02-28 02:10:18 +0600

    import images in Index. from public folder

--- src/Index.jsx
+++ src/Index.jsx
@@ -1,5 +1,12 @@
 import { AnimatePresence } from "framer-motion";
 import { useState } from "react";
+import { motion } from "framer-motion";
+
+import image1 from "../public/watch1.png";
+import image2 from "../public/watch2.png";
+import image3 from "../public/watch3.png";
+import image4 from "../public/watch4.png";
+import image5 from "../public/watch_banner.png";
 
 
 const Index = () => {
@@ -10,8 +17,13 @@ const Index = () => {
                 <AnimatePresence>
                     {
                         loading == true ?
-                        <>
-                        </>
+                        <div>
+                        <motion.img src={image1}></motion.img>
+                        <motion.img src={image2}></motion.img>
+                        <motion.img src={image3}></motion.img>
+                        <motion.img src={image4}></motion.img>
+                        <motion.img src={image5}></motion.img>
+                        </div>
                         :
                         "No animation"
                     }
