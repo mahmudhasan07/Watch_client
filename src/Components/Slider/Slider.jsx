74c3f716674e8ca0bb56bd082ca885745cf6042f
Mahmudhasan <mahmudhasan.hb@gmail.com>
2024-02-29 03:22:09 +0600

    Added to try swiper with animation

--- src/Components/Slider/Slider.jsx
+++ src/Components/Slider/Slider.jsx
@@ -1,9 +1,30 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Scrollbar, A11y, Mousewheel } from 'swiper/modules';
 
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import "swiper/css/mousewheel"
 
 const Slider = () => {
     return (
         <section>
-            
+            <Swiper
+        direction={'vertical'}
+        slidesPerView={1}
+        spaceBetween={30}
+        mousewheel={true}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Mousewheel, Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+            <img src="https://i.ibb.co/c1p1SWB/swiper2.jpg" alt="" />
+        </SwiperSlide>
+      </Swiper>
         </section>
     );
 };
