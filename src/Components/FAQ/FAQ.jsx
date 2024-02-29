9574704c104c99e6c0355e8aa93c0ee03999f818
Mahmudhasan <mahmudhasan.hb@gmail.com>
2024-02-29 16:26:03 +0600

    Added AOS for scroll animation

--- src/Components/FAQ/FAQ.jsx
+++ src/Components/FAQ/FAQ.jsx
@@ -4,16 +4,34 @@ const FAQ = () => {
     return (
         <section className="my-10">
             <h1 className="text-5xl font-bold text-center text-[#515839]">Frequently <span className="text-[#FC6F2F]">Asked</span> Questions</h1>
-            <div>
-                <div className="relative py-10">
+            <div className="flex justify-around py-10">
+                <div className="relative w-1/3">
                     <img id="FAQimg" src="https://i.ibb.co/BPY2MQs/FAQ1.jpg" className="w-96 object-cover aspect-square rounded-xl border-8" alt="" />
                     <img src="https://i.ibb.co/866fRLR/FAQ2.jpg" className="w-40 aspect-square object-cover border-4 border-white rounded-xl absolute top-[70%] left-80" alt="" />
                 </div>
-                <div className="">
-                    <details className="collapse bg-base-200">
-                        <summary className="collapse-title text-xl font-medium">Click to open/close</summary>
-                        <div className="collapse-content">
-                            <p>content</p>
+                <div className="space-y-2 w-1/3">
+                    <details className="collapse border-2 text-white bg-[#515839]">
+                        <summary className="collapse-title text-xl font-medium">Why you purchase product from our shop??</summary>
+                        <div className="collapse-content bg-[#FC6F2F]">
+                            <p>We provide genuine watches which are imported from USA, US, Canada, China , China</p>
+                        </div>
+                    </details>
+                    <details className="collapse text-white border-2 bg-[#515839]">
+                        <summary className="collapse-title text-xl font-medium">Why you purchase product from our shop??</summary>
+                        <div className="collapse-content bg-[#FC6F2F]">
+                            <p>We provide genuine watches which are imported from USA, US, Canada, China , China</p>
+                        </div>
+                    </details>
+                    <details className="collapse text-white bg-[#515839] border-2">
+                        <summary className="collapse-title text-xl font-medium">Why you purchase product from our shop??</summary>
+                        <div className="collapse-content bg-[#FC6F2F]">
+                            <p>We provide genuine watches which are imported from USA, US, Canada, China , China</p>
+                        </div>
+                    </details>
+                    <details className="collapse text-white bg-[#515839] border-2">
+                        <summary className="collapse-title text-xl font-medium">Why you purchase product from our shop??</summary>
+                        <div className="collapse-content bg-[#FC6F2F]">
+                            <p>We provide genuine watches which are imported from USA, US, Canada, China , China</p>
                         </div>
                     </details>
                 </div>
