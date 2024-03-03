e5e4833ebb6be8ead7a5327c31b58bf473a3fe97
Mahmudhasan <mahmudhasan.hb@gmail.com>
2024-03-03 13:21:45 +0600

    add key in animation

--- src/Components/Watchs/Card.jsx
+++ src/Components/Watchs/Card.jsx
@@ -1,11 +1,11 @@
 import Aos from 'aos';
 import React, { useEffect, useRef, useState } from 'react';
-import { motion, useInView } from 'framer-motion';
+import { animate, motion, useInView } from 'framer-motion';
 
 const Card = ({ card, id }) => {
     console.log(id);
     return (
-        <motion.div initial={{ x: -800 }}   animate={{x:0}}  viewport={{ once: false, amount: 1 }}
+        <motion.div initial={{ y: -80, opacity:0 }} animate={{y:0, opacity:1}}  transition={{delay:1.5, duration:1.5,when:}} viewport={{ once: false, amount: 1 }}
             className={`w-72 border-2 text-white space-y-2 rounded-xl`}>
             <img src={card?.image} alt="" />
             <h1 className='text-2xl font-bold'>{card?.title}</h1>
