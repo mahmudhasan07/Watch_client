0bece40a08a2cbd217b5ec7404013eb4bc441419
Mahmudhasan <mahmudhasan.hb@gmail.com>
2024-03-12 20:09:07 +0600

    working on best

--- src/Components/WhyUs/WhyUs.jsx
+++ src/Components/WhyUs/WhyUs.jsx
@@ -2,9 +2,14 @@ import React from 'react';
 
 const WhyUs = () => {
     return (
-        <div>
-            
-        </div>
+        <section>
+            <div>
+                <div>
+                    <img src="https://i.ibb.co/PjqqfxS/Screenshot-2024-03-12-172217-removebg-preview.png" className='w-40' alt="" />
+                </div>
+                <div className=''></div>
+            </div>
+        </section>
     );
 };
 
