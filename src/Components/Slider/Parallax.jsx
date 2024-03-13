7e0ec61fcef53847164b9b2cb4f1e33b2d9d2e5c
Mahmudhasan <mahmudhasan.hb@gmail.com>
2024-03-14 01:40:19 +0600

    working on parallax section

--- src/Components/Slider/Parallax.jsx
+++ src/Components/Slider/Parallax.jsx
@@ -6,12 +6,15 @@ const Parallax = () => {
     return (
         <section>
             <div>
-            <ParallaxBanner
-                layers={[{ image: 'https://i.ibb.co/QjFYQJv/swiper.jpg', speed: -15 }]}
-                className="aspect-[2/1] object-cover"
-            />
+                <ParallaxBanner
+                    layers={[
+                        { image: 'https://i.ibb.co/QjFYQJv/swiper.jpg', speed: -15 },
+                        { image: 'https://i.ibb.co/c1p1SWB/swiper2.jpg', speed: -10 },
+                    ]}
+                    className="aspect-[2/1] object-cover"
+                />
             </div>
-            <div>
+            {/* <div>
             <ParallaxBanner
                 layers={[{ image: 'https://i.ibb.co/c1p1SWB/swiper2.jpg', speed: -15 }]}
                 className="aspect-[2/1] object-cover "
@@ -22,7 +25,7 @@ const Parallax = () => {
                 layers={[{ image: 'https://i.ibb.co/F8Xy15N/parallax.jpg', speed: -15 }]}
                 className="aspect-[2/1] object-cover"
             />
-            </div>
+            </div> */}
         </section>
     );
 };
