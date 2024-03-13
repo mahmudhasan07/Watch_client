b15aadd66e18ae4c085718fb655d35fd44165ab1
Mahmudhasan <mahmudhasan.hb@gmail.com>
2024-03-14 01:43:25 +0600

    fix parallax system

--- src/Components/Slider/Parallax.jsx
+++ src/Components/Slider/Parallax.jsx
@@ -6,15 +6,12 @@ const Parallax = () => {
     return (
         <section>
             <div>
-                <ParallaxBanner
-                    layers={[
-                        { image: 'https://i.ibb.co/QjFYQJv/swiper.jpg', speed: -15 },
-                        { image: 'https://i.ibb.co/c1p1SWB/swiper2.jpg', speed: -10 },
-                    ]}
-                    className="aspect-[2/1] object-cover"
-                />
+            <ParallaxBanner
+                layers={[{ image: 'https://i.ibb.co/QjFYQJv/swiper.jpg', speed: -15 }]}
+                className="aspect-[2/1] object-cover"
+            />
             </div>
-            {/* <div>
+            <div>
             <ParallaxBanner
                 layers={[{ image: 'https://i.ibb.co/c1p1SWB/swiper2.jpg', speed: -15 }]}
                 className="aspect-[2/1] object-cover "
@@ -25,7 +22,7 @@ const Parallax = () => {
                 layers={[{ image: 'https://i.ibb.co/F8Xy15N/parallax.jpg', speed: -15 }]}
                 className="aspect-[2/1] object-cover"
             />
-            </div> */}
+            </div>
         </section>
     );
 };
