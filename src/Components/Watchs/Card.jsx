ae39ef5c780699a8ce8d9f3f122a0baa676c3a72
Mahmudhasan <mahmudhasan.hb@gmail.com>
2024-03-17 01:51:17 +0600

    Added animation in info section

--- src/Components/Watchs/Card.jsx
+++ src/Components/Watchs/Card.jsx
@@ -16,12 +16,12 @@ const Card = ({ card, id }) => {
 
            id='card' className={`card w-96 h-96   text-white space-y-2 rounded-xl`}>
             <img src={card?.Watch_image} id='card-img' className='h-96 w-full object-cover rounded-lg' alt="" />
-            <div id='cardinfo' className='px-1 text-center'>
+            <div id='cardinfo' className=' pt-5 text-center'>
                 <h1 className='text-xl p-1 font-semibold'>Model: {card?.Watch_name}</h1>
                 <h1 className='text-xl p-1 font-semibold'>Brand: {card?.Watch_brand}</h1>
                 <h1 className='text-xl p-1 font-semibold'>Gender: {card?.Watch_gender}</h1>
                 <h1 className='text-xl p-1 font-semibold'>Price: {card?.Watch_price}</h1>
-                <button onClick={()=> navigate(`model=${card?.Watch_name}`)} className='btn text-white font-semibold text-lg' id='detailsBTN'>Details</button>
+                <button onClick={()=> navigate(`model=${card?.Watch_name}`)} className='btn border-none text-white font-semibold text-lg' id='detailsBTN'>Details</button>
             </div>
 
         </div>
--- src/Components/Watchs/CardInfo.jsx
+++ src/Components/Watchs/CardInfo.jsx
@@ -1,28 +1,51 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import useAxios from '../Axios/useAxios';
+import { motion } from "framer-motion";
 
 const CardInfo = () => {
     const id = useParams()
+    const [array, setarray] = useState();
+    // const [split, setsplit] = useState();
+    const [details, setdetails] = useState();
     const split = id?.id.split('=')
-    console.log(split);
+    // console.log(split);
     const axiosLink = useAxios()
     useEffect(() => {
         axiosLink.get(`/watches/${split[1]}`)
         .then(res=>{
-            console.log(res.data);
+            // console.log(res.data);
+            setarray(res?.data)
+            // const data = res?.data?.Watch_note.split(".")
+            // console.log(data);
+            setdetails(res?.data?.Watch_note.split("."))
         })
         .catch(error=>{
             console.log(error);
         })
     }, [axiosLink, split]);
-    return (
-        <section>
-            <h1 className='text-4xl font-semibold text-center text-[#515839]'>Product <span className='text-[#FC6F2F]'>Information</span></h1>
-            <div>
-                <div>
 
-                </div>
+    console.log(details);
+    return (
+        <section className='my-10'>
+            <h1 className='text-4xl font-bold text-center text-[#515839]'>Product <span className='text-[#FC6F2F]'>Information</span></h1>
+            <div className='flex justify-around overflow-hidden flex-wrap lg:my-14 md:my-9 my-6'>
+                <motion.div initial={{x:"-200%", opacity:0}} animate={{x:0, opacity:1}} transition={{delay:1, duration:1.5, ease:"backOut"}}>
+                    <img src={array?.Watch_image} className='w-80 rounded-2xl' alt="" />
+                </motion.div>
+                <motion.div initial={{x:"200%", opacity:0}} animate={{x:0, opacity:1}} transition={{delay:1, duration:1.5, ease:"backOut"}} 
+                className='text-xl text-white font-semibold'>
+                    <h1 className='text-[#FC6F2F]'>Name : <span className='text-[#515839]'> {array?.Watch_name}</span></h1>
+                    <h1 className='text-[#FC6F2F]'>Brand : <span className='text-[#515839]'> {array?.Watch_brand}</span></h1>
+                    <h1 className='text-[#FC6F2F]'>Gender :  <span className='text-[#515839]'> {array?.Watch_gender}</span></h1>
+                    <h1 className='text-[#FC6F2F]'>Price : <span className='text-[#515839]'> {array?.Watch_price}</span></h1>
+                    <div>
+                        <h1 className='text-xl text-[#FC6F2F] font-semibold'>Watch Details</h1>
+                        {
+                            details?.map((element,idx)=><li className='text-lg text-[#515839]' key={idx}>{element}</li>)
+                        }
+                    </div>
+                </motion.div>
             </div>
         </section>
     );
