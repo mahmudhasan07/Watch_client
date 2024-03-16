66e966d4c74c7856e177ebb2b185f4026f4bb0ff
Mahmudhasan <mahmudhasan.hb@gmail.com>
2024-03-16 12:35:48 +0600

    working on create product info route

--- src/Components/Watchs/Card.jsx
+++ src/Components/Watchs/Card.jsx
@@ -2,24 +2,26 @@ import Aos from 'aos';
 import React, { useEffect, useRef, useState } from 'react';
 import { animate, motion, useInView } from 'framer-motion';
 import "./Card.css"
+import { useNavigate } from 'react-router-dom';
 
 const Card = ({ card, id }) => {
     // console.log(id);
+    const navigate = useNavigate()
     const note = card?.Watch_note
     // console.log(note);
-    const newarray = note.split(".")
+    // const newarray = note.split(".")
     // console.log(newarray);
     return (
         <div
 
            id='card' className={`card w-96 h-96   text-white space-y-2 rounded-xl`}>
             <img src={card?.Watch_image} id='card-img' className='h-96 w-full object-cover rounded-lg' alt="" />
-            <div id='cardinfo' className='px-1'>
+            <div id='cardinfo' className='px-1 text-center'>
                 <h1 className='text-xl p-1 font-semibold'>Model: {card?.Watch_name}</h1>
                 <h1 className='text-xl p-1 font-semibold'>Brand: {card?.Watch_brand}</h1>
                 <h1 className='text-xl p-1 font-semibold'>Gender: {card?.Watch_gender}</h1>
                 <h1 className='text-xl p-1 font-semibold'>Price: {card?.Watch_price}</h1>
-                <button className='btn text-white font-semibold text-lg' id='detailsBTN'>Details</button>
+                <button onClick={()=> navigate(`model=${card?.Watch_name}`)} className='btn text-white font-semibold text-lg' id='detailsBTN'>Details</button>
             </div>
 
         </div>
