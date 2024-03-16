5ed93c8c5202bd254ba1baac3419250e6d2a5887
Mahmudhasan <mahmudhasan.hb@gmail.com>
2024-03-17 01:03:53 +0600

    create axios link

--- src/Components/Watchs/CardInfo.jsx
+++ src/Components/Watchs/CardInfo.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import useAxios from '../Axios/useAxios';
 
 const CardInfo = () => {
     const id = useParams()
     const split = id?.id.split('=')
     console.log(split);
+    const axiosLink = useAxios()
+    useEffect(() => {
+        axiosLink.get(`/watches/${split[1]}`)
+        .then(res=>{
+            console.log(res.data);
+        })
+        .catch(error=>{
+            console.log(error);
+        })
+    }, [axiosLink, split]);
     return (
         <section>
             <h1 className='text-4xl font-semibold text-center text-[#515839]'>Product <span className='text-[#FC6F2F]'>Information</span></h1>
