a59b14a204bfe417a3e582c97ddc2909efc4a483
Mahmudhasan <mahmudhasan.hb@gmail.com>
2024-03-17 17:35:21 +0600

    error hosting problem

--- src/Components/Axios/useAxios.jsx
+++ src/Components/Axios/useAxios.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 export const AxiosSecure = axios.create({
     // baseURL: "http://localhost:3000",
     baseURL: "https://watch-server-07.vercel.app",
-    withCredentials: true   
+    // withCredentials: true   
 })
 const useAxios = () => {
 return AxiosSecure
