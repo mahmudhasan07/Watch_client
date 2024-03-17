cd0d107fd3204d9eb33254a21599dd51cf094969
Mahmudhasan <mahmudhasan.hb@gmail.com>
2024-03-17 17:27:29 +0600

    host with firebase

--- src/Components/Axios/useAxios.jsx
+++ src/Components/Axios/useAxios.jsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export const AxiosSecure = axios.create({
-    baseURL: "http://localhost:3000",
+    // baseURL: "http://localhost:3000",
+    baseURL: "https://watch-server-07.vercel.app",
     withCredentials: true   
 })
 const useAxios = () => {
--- src/Components/Trending/Trending.jsx
+++ src/Components/Trending/Trending.jsx
@@ -16,15 +16,15 @@ const Trending = () => {
         })
     }, [axiosLink]);
     return (
-        <section>
+        <section className='lg:my-12 md:my-8 my-6'>
             <h1 className='text-4xl text-center my-5 font-bold text-[#515839]'>Trending <span className='text-[#FC6F2F]'>Watches</span></h1>
 
-            <marquee  className="bg-[#515839] text-2xl p-1 text-black rounded-lg mx-8">
+            <marquee  className="bg-[#515839] text-2xl my-5 p-1 text-black rounded-lg lg:mx-8 md:mx-6 mx-2">
                 <h1 className=''>Highlight watches worn by celebrities or influences, as this can significantly influence purchasing decisions. Include images or quotes from celebrities to add credibility and appeal.</h1>
             </marquee>
-            <div>
+            <div className='flex justify-center gap-5 flex-wrap'>
                 {
-                    array?.map((element, idx)=> <Card card={element} key={idx} id={idx}></Card>)
+                    array.slice(0,4)?.map((element, idx)=> <Card card={element} key={idx} id={idx}></Card>)
                 }
                 
             </div>
