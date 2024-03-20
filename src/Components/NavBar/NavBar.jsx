e3dbda1dad8894a7fab70f4b2a8f70dbd9db4e00
Mahmudhasan <mahmudhasan.hb@gmail.com>
2024-03-21 02:12:54 +0600

    final resonsive complete

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -25,12 +25,12 @@ const NavBar = () => {
 
     return (
         <section className=' bg-gray-950'>
-            <div className='flex flex-wrap justify-around'>
+            <div className='flex flex-wrap justify-around  '>
                 <div>
                     <img src={logo} className='w-40 h-24' alt="" />
                 </div>
                 <div className='my-auto p-2 '>
-                    <ul className='text-[#515839]  text-xl font-extrabold flex flex-wrap gap-10'>
+                    <ul className='text-[#515839]  text-xl font-extrabold flex flex-wrap justify-center lg:gap-10 md:gap-7 gap-5'>
                         <NavLink className={activeRoute} to={`/`}><motion.li className='navli' variants={animation} initial="initial" animate="animate" transition={{ delay: 5.1 , duration: 1.5, ease: "backOut" }}>Home</motion.li></NavLink>
                         <NavLink className={activeRoute} to={`/watches`}><motion.li className='navli' variants={animation} initial="initial" animate="animate" transition={{ delay: 5.3, duration: 1.5, ease: "backOut" }}>Watches</motion.li></NavLink>
                         <NavLink className={activeRoute} to={`/added-watches`}><motion.li className='navli' variants={animation} initial="initial" animate="animate" transition={{ delay: 5.5, duration: 1.5, ease: "backOut" }}>Add Watches</motion.li></NavLink>
